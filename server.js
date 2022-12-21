b232afb0dddf83de55b1f60e68a25ef0efd68eca
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2022-12-21 07:37:48 -0500

    add: start routes for idx, show, new, edit page

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
+//Dependencies
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Port = 3000;
-
 require('dotenv').config();
+
+//Middleware
 
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended: true}));
 app.use('/public', express.static('public'));
-
-//Mount Middleware
-
 const PORT = process.env.PORT;
 const DATABASE_URI = process.env.DATABASE_URI;
 mongoose.connect(DATABSE_URI);
 const db = mongoose.connection;
 
 //Index
+app.get('/products', (req, res)=>{
+    Product.find({}, (err, foundProducts) =>{
+        res.render('index.ejs', {products: foundProducts});
+    });
+});
 //New
 //Delete
 //Update
