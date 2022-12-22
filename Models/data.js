a34d6c8e7a95c307e4b5e7bc2b1fe46ae5fb0692
Michael Mangan <michaelmangan@Michaels-MBP.attlocal.net>
2022-12-22 13:42:19 -0500

    add working index route

--- Models/data.js
+++ Models/data.js
@@ -3,19 +3,19 @@ module.exports = [
       name: '001',
       description: 'The first shirt style that I produced. This shirt can be made in any color of your choice.',
       img: 'https://i.imgur.com/f2gV9t6.png',
-      price: $55.00,
+      price: 55,
       qty: 12
     }, {
       name: '002',
       description: "This is a traditional spiral fold and can be used with any color pattern.",
       img: 'https://i.imgur.com/9Ixfy5y.jpg',
-      price: $45.00,
+      price: 45,
       qty: 10
     }, {
       name: '003',
       description: 'This is a reverse process tie dye which requires extra steps. This shirt can be done with up to 4 colors of your choice.',
       img: 'https://i.imgur.com/VSCzPUE.png',
-      price: $75.00,
+      price: 75,
       qty: 8
     }
   ]
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const Product = require('./models/productSchema.js')
 const Port = 3000;
 require('dotenv').config();
 
@@ -11,15 +12,30 @@ require('dotenv').config();
 app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended: true}));
 app.use('/public', express.static('public'));
+
 const PORT = process.env.PORT;
 const DATABASE_URI = process.env.DATABASE_URI;
-mongoose.connect(DATABSE_URI);
+mongoose.connect(DATABASE_URI);
 const db = mongoose.connection;
+db.on('error', (err)=>console.log('error occurred with MongoDB: ' + err.message));
+db.on('connected', ()=>console.log(`successful connection to MongoDB on ${db.host}:${db.port}`));
+
+const productSeed = require('./models/data.js');
+// const { $where } = require('./models/productSchema.js');
+
+//Seed Route
+app.get('/products/seed', (req, res) =>{
+    Product.deleteMany({}, (err) =>{
+        Product.create(productSeed, (err, data) =>{
+            res.redirect('/products');
+        });
+    });
+});
 
 //Index
-app.get('/products', (req, res)=>{
-    Product.find({}, (err, foundProducts) =>{
-        res.render('index.ejs', {products: foundProducts});
+app.get('/products', (req, res) =>{
+    Product.find({}, (err, foundProduct) =>{
+        res.render('index.ejs', {products: foundProduct});
     });
 });
 //New
@@ -28,6 +44,11 @@ app.get('/products', (req, res)=>{
 //Create
 //Edit
 //Show
+app.get('/products/:id', (req, res)=>{
+    Product.findById(req.params.id, (err, foundProduct) =>{
+        res.render('show.ejs', {foundProduct});
+    });
+});
 
 //App Listener
 app.listen(Port, () =>{
